e05ace8460bad13b5f047aea3de4f5e7042c3779
Kevin Liang <kevinpliang@gmail.com>
2021-05-12 00:29:53 -0400

    richard boss fight cutscene

--- world/Richard.gd
+++ world/Richard.gd
@@ -6,25 +6,24 @@ onready var head = $HeadSprite
 onready var sprite = $BodySprite
 
 var canTalk = false
+var cutscene = false
 
 var dialogue = ["...", ".....", ".......", "my little bro...\nhasn't come back up.", "he's a big baby..\ni'm worried."]
-var d_speed = [1, 1, 1, 0.4, 0.4]
+var d_speed = [0.8, 0.8, 0.8, 0.4, 0.4]
 var d_index = 0
 
 func _process(delta):
 	if d_index >= dialogue.size():
 		canTalk = false
-	
-	if canTalk:
-		if !animation.is_playing():
+	if !animation.is_playing() and !cutscene:		
+		if canTalk:		
 			label.text = "[E]"
 			label.percent_visible = 1
-	else:
-		label.text = ""
-		label.percent_visible = 0
+		else:
+			label.text = ""
+			label.percent_visible = 0
 
 func _input(event):
-	print(event)
 	if Input.is_action_pressed("interact") and canTalk:
 		label.text = dialogue[d_index]
 		animation.playback_speed = d_speed[d_index]
@@ -46,13 +45,16 @@ func _on_interactRange_area_exited(area):
 		yield(animation, "animation_finished")
 	if area.is_in_group("player"):
 		if d_index >= dialogue.size():
-			yield(get_tree().create_timer(1), "timeout")
+			cutscene = true
+			yield(get_tree().create_timer(1.5), "timeout")
 			head.hide()
 			label.text = "wait..."
 			animation.playback_speed = 0.5
 			animation.play("show_label")
 			yield(animation, "animation_finished")
-			label.text = "where'd you get that shirt?"
+			sprite.play("realize")
+			yield(get_tree().create_timer(1.5), "timeout")
+			label.text = "where'd you get\nthat shirt?"
 			animation.play("show_label")
 			yield(animation, "animation_finished")
 			startFight()
